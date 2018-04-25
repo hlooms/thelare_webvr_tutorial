a2724495591ffc48cf618c4c031df5e597de5de0
hlooms <38737240+hlooms@users.noreply.github.com>
2018-04-25 17:26:28 -0400

    Update theLareCommonCode.js

--- public/theLareCommonCode.js
+++ public/theLareCommonCode.js
@@ -11,14 +11,14 @@ var drawingRoom = vRViewPlayer('#first-vr-example', {
 window.addEventListener('load', drawingRoom);
 
 var roomDrawing = vRViewPlayer('#second-vr-example', {
-  image: 'https://fscl01.fonpit.de/userfiles/4155894/image/photo-sphere-w628.jpg',
+  image: 'https://q3fb03rfy3f4ahuzu2uy6e11-wpengine.netdna-ssl.com/wp-content/uploads/2015/09/photosphere-android.jpg',
   is_stereo: false,
 });
 
 window.addEventListener('load', roomDrawing);
 
 var boxRoomTemplate = vRViewPlayer('#third-vr-example', {
-  image: 'https://q3fb03rfy3f4ahuzu2uy6e11-wpengine.netdna-ssl.com/wp-content/uploads/2015/09/photosphere-android.jpg',
+  image: '',
   is_stereo: false,
 });
 
